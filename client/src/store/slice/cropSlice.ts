e962a7f4078f7390a21f16427a6bd6a389e876a0
Foridul35962 <foridulislam35962@gmail.com>
2026-08-03 21:45:32 +0600

    feat: add crop types and recommendation interfaces for improved type safety

--- client/src/store/slice/cropSlice.ts
+++ client/src/store/slice/cropSlice.ts
@@ -1,3 +1,4 @@
+import { CreateCropRequest, CreateRecommendationTypes, CropDetails, CropRecommendation, GetAllCropData, UpdateRecommendationTypes } from "@/types/cropTypes";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios, { AxiosError } from "axios";
 
@@ -20,9 +21,9 @@ export const createCrop = createAsyncThunk(
 
 export const getAllCrops = createAsyncThunk(
     "crop/all",
-    async(params:{name?:string, category?:string, page:number }, {rejectWithValue})=>{
+    async (params: { name?: string, category?: string, page: number }, { rejectWithValue }) => {
         try {
-            const res = await axios.get(`${SERVER_URL}/all`,{
+            const res = await axios.get(`${SERVER_URL}/all`, {
                 withCredentials: true,
                 params
             })
@@ -36,10 +37,10 @@ export const getAllCrops = createAsyncThunk(
 
 export const getCropDetails = createAsyncThunk(
     "crop/details",
-    async({cropId}:{cropId:string},{rejectWithValue})=>{
+    async ({ cropId }: { cropId: string }, { rejectWithValue }) => {
         try {
             const res = await axios.get(`${SERVER_URL}/details/${cropId}`,
-                {withCredentials: true}
+                { withCredentials: true }
             )
             return res.data
         } catch (error) {
@@ -51,10 +52,10 @@ export const getCropDetails = createAsyncThunk(
 
 export const updateCrop = createAsyncThunk(
     "crop/update",
-    async({cropId, data}:{cropId:string, data:FormData},{rejectWithValue})=>{
+    async ({ cropId, data }: { cropId: string, data: FormData }, { rejectWithValue }) => {
         try {
             const res = await axios.patch(`${SERVER_URL}/update/${cropId}`, data,
-                {withCredentials: true}
+                { withCredentials: true }
             )
             return res.data
         } catch (error) {
@@ -66,9 +67,9 @@ export const updateCrop = createAsyncThunk(
 
 export const deleteCrop = createAsyncThunk(
     "crop/delete",
-    async({cropId}:{cropId:string},{rejectWithValue})=>{
+    async ({ cropId }: { cropId: string }, { rejectWithValue }) => {
         try {
-            const res = await axios.delete(`${SERVER_URL}/delete/${cropId}`,{
+            const res = await axios.delete(`${SERVER_URL}/delete/${cropId}`, {
                 withCredentials: true
             })
             return res.data
@@ -81,10 +82,10 @@ export const deleteCrop = createAsyncThunk(
 
 export const createRecommendation = createAsyncThunk(
     "crop/recommendation",
-    async(data:CreateRecommendationTypes, {rejectWithValue})=>{
+    async (data: CreateRecommendationTypes, { rejectWithValue }) => {
         try {
             const res = await axios.post(`${SERVER_URL}/create/recommendation`, data,
-                {withCredentials:true}
+                { withCredentials: true }
             )
             return res.data
         } catch (error) {
@@ -96,10 +97,10 @@ export const createRecommendation = createAsyncThunk(
 
 export const updateRecommendation = createAsyncThunk(
     "crop/updaterecommendation",
-    async(data:UpdateRecommendationTypes, {rejectWithValue})=>{
+    async (data: UpdateRecommendationTypes, { rejectWithValue }) => {
         try {
             const res = await axios.patch(`${SERVER_URL}/update/recommendation`, data,
-                {withCredentials:true}
+                { withCredentials: true }
             )
             return res.data
         } catch (error) {
@@ -111,10 +112,10 @@ export const updateRecommendation = createAsyncThunk(
 
 export const deleteRecommendation = createAsyncThunk(
     "crop/deleterecommendation",
-    async({cropRecommendationId}:{cropRecommendationId:string}, {rejectWithValue})=>{
+    async ({ cropRecommendationId }: { cropRecommendationId: string }, { rejectWithValue }) => {
         try {
             const res = await axios.delete(`${SERVER_URL}/delete/recommendation/:${cropRecommendationId}`,
-                {withCredentials:true}
+                { withCredentials: true }
             )
             return res.data
         } catch (error) {
@@ -124,20 +125,150 @@ export const deleteRecommendation = createAsyncThunk(
     }
 )
 
-interface initialStateType{
-    cropLoading:boolean
+interface initialStateType {
+    cropLoading: boolean
+    allCrops: GetAllCropData
+    cropDetails: CropDetails
 }
 
-const initialState:initialStateType={
-    cropLoading:false
+const initialState: initialStateType = {
+    cropLoading: false,
+    allCrops: {
+        crops: [],
+        pagination: {
+            currentPage: 0,
+            hasNextPage: false,
+            hasPrevPage: false,
+            limit: 0,
+            totalCrops: 0,
+            totalPages: 0
+        }
+    },
+    cropDetails: {
+        crop: null,
+        recommendation: null
+    }
 }
 
 const cropSlice = createSlice({
-    name:"crop",
+    name: "crop",
     initialState,
-    reducers:{},
-    extraReducers:(builder)=> {
-        
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(createCrop.pending, (state) => {
+                state.cropLoading = true
+            })
+            .addCase(createCrop.fulfilled, (state, action) => {
+                state.cropLoading = false
+                if (state.allCrops.crops.length > 0) {
+                    state.allCrops.crops = [...action.payload.data, state.allCrops.crops]
+                } else {
+                    state.allCrops.crops = [action.payload.data]
+                }
+            })
+            .addCase(createCrop.rejected, (state) => {
+                state.cropLoading = false
+            })
+        builder
+            .addCase(getAllCrops.pending, (state) => {
+                state.cropLoading = true
+            })
+            .addCase(getAllCrops.fulfilled, (state, action) => {
+                state.cropLoading = false
+                state.allCrops = action.payload.data
+            })
+            .addCase(getAllCrops.rejected, (state) => {
+                state.cropLoading = false
+            })
+        builder
+            .addCase(getCropDetails.pending, (state) => {
+                state.cropLoading = true
+            })
+            .addCase(getCropDetails.fulfilled, (state, action) => {
+                state.cropLoading = false
+                state.cropDetails = action.payload.data
+            })
+            .addCase(getCropDetails.rejected, (state) => {
+                state.cropLoading = false
+            })
+        builder
+            .addCase(updateCrop.pending, (state) => {
+                state.cropLoading = true
+            })
+            .addCase(updateCrop.fulfilled, (state, action) => {
+                state.cropLoading = false
+                const crop: CreateCropRequest = action.payload.data
+                state.cropDetails.crop = crop
+                const idx = state.allCrops.crops.findIndex((c) => c._id === crop._id)
+                if (idx > -1) {
+                    state.allCrops.crops[idx] = crop
+                }
+            })
+            .addCase(updateCrop.rejected, (state) => {
+                state.cropLoading = false
+            })
+        builder
+            .addCase(deleteCrop.pending, (state) => {
+                state.cropLoading = true
+            })
+            .addCase(deleteCrop.fulfilled, (state, action) => {
+                state.cropLoading = false
+                const cropId = action.payload.data
+                if (state.allCrops.crops) {
+                    state.allCrops.crops = state.allCrops.crops.filter((crop) => crop._id !== cropId)
+                }
+                if (state.cropDetails.crop?._id === cropId) {
+                    state.cropDetails.crop = null
+                    state.cropDetails.recommendation = null
+                }
+            })
+            .addCase(deleteCrop.rejected, (state) => {
+                state.cropLoading = false
+            })
+        builder
+            .addCase(createRecommendation.pending, (state) => {
+                state.cropLoading = true
+            })
+            .addCase(createRecommendation.fulfilled, (state, action) => {
+                state.cropLoading = false
+                const recommendation: CropRecommendation = action.payload.data
+                if (state.cropDetails.crop?._id === recommendation._id) {
+                    state.cropDetails.recommendation = recommendation
+                }
+            })
+            .addCase(createRecommendation.rejected, (state) => {
+                state.cropLoading = false
+            })
+        builder
+            .addCase(updateRecommendation.pending, (state) => {
+                state.cropLoading = true
+            })
+            .addCase(updateRecommendation.fulfilled, (state, action) => {
+                state.cropLoading = false
+                const recommendation: CropRecommendation = action.payload.data
+                if (state.cropDetails.crop?._id === recommendation._id) {
+                    state.cropDetails.recommendation = recommendation
+                }
+            })
+            .addCase(updateRecommendation.rejected, (state) => {
+                state.cropLoading = false
+            })
+        builder
+            .addCase(deleteRecommendation.pending, (state) => {
+                state.cropLoading = true
+            })
+            .addCase(deleteRecommendation.fulfilled, (state, action) => {
+                state.cropLoading = false
+                const { cropId, cropRecommendationId } = action.payload.data
+                if (state.cropDetails.crop?._id === cropId &&
+                    state.cropDetails.recommendation?._id === cropRecommendationId) {
+                    state.cropDetails.recommendation = null
+                }
+            })
+            .addCase(deleteRecommendation.rejected, (state) => {
+                state.cropLoading = false
+            })
     },
 })
 
