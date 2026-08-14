bd45a34aa02becb8231b1955a4b4140aae99f97a
Foridul35962 <foridulislam35962@gmail.com>
2026-08-14 22:42:25 +0600

    feat: add routes for aratdar received orders and retailer placed orders with details

--- server/src/controllers/order.controller.js
+++ server/src/controllers/order.controller.js
@@ -249,3 +249,260 @@ export const getAratdarPlacedOrderDetails = AsyncHandler(async (req, res) => {
             new ApiResponse(200, order, "order details fetch successfully")
         )
 })
+
+export const getAratdarReceivedOrder = AsyncHandler(async (req, res) => {
+    const page = Number(req.query.page) || 1;
+    const limit = 15;
+    const skip = (page - 1) * limit;
+
+    const userId = req.user._id;
+
+    const [orders, totalOrders] = await Promise.all([
+        Orders.find({
+            sellerId: userId,
+            sellerRole: "aratdar"
+        })
+            .select(
+                "inventoryId quantity unit totalAmount status createdAt"
+            )
+            .populate({
+                path: "inventoryId",
+                select: "productName image.url"
+            })
+            .sort({
+                createdAt: -1
+            })
+            .skip(skip)
+            .limit(limit),
+
+
+        Orders.countDocuments({
+            sellerId: userId,
+            sellerRole: "aratdar"
+        })
+    ]);
+
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    orders,
+                    pagination: {
+                        currentPage: page,
+                        totalPages: Math.ceil(totalOrders / limit),
+                        totalOrders,
+                        limit
+                    }
+                },
+                "Received orders fetched successfully"
+            )
+        );
+})
+
+export const getAratdarReceiveOrderDetails = AsyncHandler(async (req, res) => {
+    const userId = req.user._id
+    const { orderId } = req.params
+    if (!orderId) {
+        throw new ApiErrors(400, "order id is required")
+    }
+
+    if (!mongoose.isValidObjectId(orderId)) {
+        throw new ApiErrors(400, "invalid order id")
+    }
+
+    const redisKey = `inventoryOrderDetails:aratdar:${orderId}`
+
+    const redisOrder = await redis.get(redisKey)
+    let order
+
+    if (redisOrder) {
+        order = JSON.parse(redisOrder)
+    } else {
+        order = await Orders.findOne({
+            _id: orderId,
+            sellerId: userId,
+            sellerRole: "aratdar"
+        })
+            .select("sellerId buyerId inventoryId quantity unit totalAmount status createdAt")
+            .populate({
+                path: "buyerId",
+                select: "name phoneNumber email district"
+            })
+            .populate({
+                path: "inventoryId",
+                select: "productName category image.url"
+            });
+
+        await redis.set(redisKey,
+            JSON.stringify(order),
+            "EX", 300
+        )
+    }
+
+    if (!order) {
+        throw new ApiErrors(404, "order is not found")
+    }
+
+    if (order.sellerId.toString() !== userId.toString()) {
+        throw new ApiErrors(403, "unauthorized access")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, order, "order details fetch successfully")
+        )
+})
+
+export const changeAratdarOrderStatus = AsyncHandler(async (req, res) => {
+    const userId = req.user._id
+    const { orderId } = req.params
+    if (!orderId) {
+        throw new ApiErrors(400, "order id is required")
+    }
+
+    const { status } = req.body
+    if (!status) {
+        throw new ApiErrors(400, "status are required")
+    }
+
+    if (!["PROCESSING", "SHIPPED", "DELIVERED"].includes(status)) {
+        throw new ApiErrors(400, "invalid status")
+    }
+
+    const order = await Orders.findOneAndUpdate(
+        {
+            _id: orderId,
+            sellerId: userId,
+            sellerRole: "aratdar"
+        },
+        {
+            status: status
+        },
+        {
+            new: true,
+        }
+    )
+
+    if (!order) {
+        throw new ApiErrors(404, "order not found")
+    }
+
+    await redis.del(`inventoryOrderDetails:aratdar:${orderId}`)
+    await redis.del(`inventoryOrderDetails:retailer:${orderId}`)
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, { orderId, status }, "order status change successfully")
+        )
+})
+
+export const getRetailerPlacedOrder = AsyncHandler(async (req, res) => {
+    const page = Number(req.query.page) || 1;
+    const limit = 15;
+    const skip = (page - 1) * limit;
+
+    const userId = req.user._id;
+
+    const [orders, totalOrders] = await Promise.all([
+        Orders.find({
+            buyerId: userId,
+            buyerRole: "retailer"
+        })
+            .select(
+                "inventoryId quantity unit totalAmount status createdAt"
+            )
+            .populate({
+                path: "inventoryId",
+                select: "productName image.url"
+            })
+            .sort({
+                createdAt: -1
+            })
+            .skip(skip)
+            .limit(limit),
+
+
+        Orders.countDocuments({
+            buyerId: userId,
+            buyerRole: "retailer"
+        })
+    ]);
+
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    orders,
+                    pagination: {
+                        currentPage: page,
+                        totalPages: Math.ceil(totalOrders / limit),
+                        totalOrders,
+                        limit
+                    }
+                },
+                "placed orders fetched successfully"
+            )
+        );
+})
+
+export const getRetailerPlacedOrderDetails = AsyncHandler(async (req, res) => {
+    const userId = req.user._id
+    const { orderId } = req.params
+    if (!orderId) {
+        throw new ApiErrors(400, "order id is required")
+    }
+
+    if (!mongoose.isValidObjectId(orderId)) {
+        throw new ApiErrors(400, "invalid order id")
+    }
+
+    const redisKey = `inventoryOrderDetails:retailer:${orderId}`
+
+    const redisOrder = await redis.get(redisKey)
+    let order
+
+    if (redisOrder) {
+        order = JSON.parse(redisOrder)
+    } else {
+        order = await Orders.findById({
+            _id: orderId,
+            buyerId: userId,
+            buyerRole: "retailer"
+        })
+            .select("sellerId buyerId inventoryId quantity unit totalAmount status createdAt")
+            .populate({
+                path: "sellerId",
+                select: "name phoneNumber email"
+            })
+            .populate({
+                path: "inventoryId",
+                select: "productName category image.url"
+            });
+        await redis.set(redisKey,
+            JSON.stringify(order),
+            "EX", 300
+        )
+    }
+
+    if (!order) {
+        throw new ApiErrors(404, "order is not found")
+    }
+
+    if (order.buyerId.toString() !== userId.toString()) {
+        throw new ApiErrors(403, "unauthorized access")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, order, "order details fetch successfully")
+        )
+})
