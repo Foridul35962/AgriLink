62fac16d02fc22391f39215f45a3cff9b1001f1d
Foridul35962 <foridulislam35962@gmail.com>
2026-09-23 23:17:22 +0600

    feat: refactor layout components to include Sidebar and improve structure

--- client/src/app/(farmer)/layout.tsx
+++ client/src/app/(farmer)/layout.tsx
@@ -1,13 +1,17 @@
+import { Sidebar } from '@/components/sidebar/Sidebar'
 import FarmerProvider from '@/providers/FarmerProvider'
 import React from 'react'
 
 const layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <>
-            <FarmerProvider>
-                {children}
-            </FarmerProvider>
-        </>
+        <div className="flex min-h-screen bg-[#f5faf6]">
+            <Sidebar />
+            <main className="flex-1 p-4 md:ml-64 md:p-6 md:pt-6">
+                <FarmerProvider>
+                    {children}
+                </FarmerProvider>
+            </main>
+        </div>
     )
 }
 
--- client/src/app/aratdar/layout.tsx
+++ client/src/app/aratdar/layout.tsx
@@ -1,13 +1,17 @@
+import { Sidebar } from '@/components/sidebar/Sidebar'
 import AratdarProvider from '@/providers/AratdarProvider'
 import React from 'react'
 
 const layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <>
-            <AratdarProvider>
-                {children}
-            </AratdarProvider>
-        </>
+        <div className="flex min-h-screen bg-[#f5faf6]">
+            <Sidebar />
+            <main className="flex-1 p-4 md:ml-64 md:p-6 md:pt-6">
+                <AratdarProvider>
+                    {children}
+                </AratdarProvider>
+            </main>
+        </div>
     )
 }
 
